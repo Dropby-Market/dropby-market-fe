07bad3eb2d75c7c2c9d8a3d14ce03104c5a805e7
gosmdochi <nohys0312@gmail.com>
2025-08-13 09:27:19 +0900

    상단요약가게정보

--- src/page/StoreDetail/StoreDetail.jsx
+++ src/page/StoreDetail/StoreDetail.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+// 이미지랑 아이콘 불러오기
 import Example from "@/assets/storeDetail/Example.png";
-import SavedIcon from "@/assets/storeDetail/SavedIcon.svg?react";
 import StarIcon from "@/assets/storeDetail/StarIcon.svg?react";
 import Horizon from "@/assets/storeDetail/Horizon.svg?react";
+import TimeIcon from "@/assets/storeDetail/TimeIcon.svg?react";
+import SpotIcon from "@/assets/storeDetail/SpotIcon.svg?react";
+import NumIcon from "@/assets/storeDetail/NumIcon.svg?react";
+
+// componets
 import SaveButton from "./components/SaveButton"
+import NumCopyButton from "./components/NumCopyButton"
 
 const StoreDetail = () => {
   const params = useParams();
@@ -18,7 +24,7 @@ const StoreDetail = () => {
         <img src={Example} alt="예시이미지" className="w-full"/>
       </div>
 
-      {/*가게 이름, 별점, 설명 UI*/}
+      {/*가게 이름, 별점, 설명*/}
       <header className="py-5">
         <div className="flex gap-1 text-xs leading-4 font-medium text-[#9CA3AF]">
           <span>천안중앙시장</span>
@@ -28,6 +34,7 @@ const StoreDetail = () => {
 
         <div className="flex justify-between">
           <h1 className="text-2xl leading-8 font-semibold">못난이 사과</h1>
+          {/* 저장 안되는 저장버튼(구현되면 지우기) */}
           <SaveButton />
         </div>
 
@@ -42,6 +49,33 @@ const StoreDetail = () => {
 
       {/*구분선*/}
       <Horizon className="w-full h-1 mx-auto"/>
+
+      {/* 간단설명 */}
+      <nav className="py-5">
+        <dl>
+          <div className="flex items-center">
+            <TimeIcon className="w-4 h-4" />
+            <dt className="pl-1.5 pr-5 text-xs leading-4 font-medium text-[#374151]">소요시간</dt>
+            <dd className="text-xs leading-4 font-medium text-[#374151]">0~5분</dd>
+          </div>
+
+          <div className="flex py-5 items-top">
+            <SpotIcon className="w-4 h-4"/>
+            <dt className="pl-1.5 pr-5 text-xs leading-4 font-medium text-[#374151]">위치정보</dt>
+            <div className="block max-w-64.25">
+              <dd className="text-xs leading-4 font-medium text-[#374151]">현재 위치에서 208m</dd>
+              <dd className="text-xs leading-4 font-medium text-[#6B7280]">충남 천안시 동남구 사직로 32 (충남 천안시 동남구 사직동 29-1)</dd>
+            </div>
+          </div>
+          <div className="flex">
+          <NumIcon />
+            <dt className="pl-1.5 pr-5 text-xs leading-4 font-medium text-[#374151]">전화번호</dt>
+            <dd className="text-xs leading-4 font-medium text-[#374151]">010-1234-4567</dd>
+            {/* 복사안되는 복사버튼(구현되면지우기) */}
+            <NumCopyButton />
+          </div>
+        </dl>
+      </nav>
     </div>
   );
 };
---
+++ src/page/StoreDetail/components/NumCopyButton.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const NumCopyButton = () => {
+
+    return (
+        <div>
+            <button className="cursor-pointer flex gap-1 text-xs leading-4 font-medium text-[#059669]">
+                복사
+            </button>
+        </div>
+    );
+};
+
+export default NumCopyButton;
